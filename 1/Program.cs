50f646334c3923aafd9635e1a953b0082ec9b417
«Nikolai» <>
2022-11-09 12:25:48 +0300

    first commit

---
+++ 1/Program.cs
@@ -0,0 +1,29 @@
+﻿int[]  numbers(int count)
+{   
+    int [] arr= new int[count];
+    for (int i = 0;i<count;i++)
+    {
+        Console.WriteLine("Введите число");
+        arr[i] = int.Parse(Console.ReadLine());
+    }
+    return arr ;
+}
+
+int counter(int[] arr1)
+{
+    int count=0;
+    for (int i = 0;i<arr1.Length;i++)
+    {
+        if (arr1[i]>0)
+        {
+            count++;
+        }
+    }
+    return count;
+
+}
+Console.WriteLine("Введите Число M");
+
+int M = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Чисел больше нуля :{counter(numbers(M))}");
